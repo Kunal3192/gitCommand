9d03097f4c2e33302a76a354895a9fd834e948cd
Kunal <kunalkumar3191@gmail.com>
2021-01-06 09:51:24 -0500

    added mul function.

--- src/practice/GitCommand.java
+++ src/practice/GitCommand.java
@@ -7,9 +7,13 @@ public class GitCommand {
     public static int calcSub(int a, int b){
         return a - b;
     }
+    public static int calcMul(int a, int b){
+        return a * b;
+    }
     public static void main(String[] args) {
         System.out.println("This is git command");
         System.out.println(calcSum(5, 6));
         System.out.println(calcSub(10, 5));
+        System.out.println(calcMul(10, 5));
     }
 }
