a061d5b0bb0c1600a246448044b6037c5eae4e87
Kunal <kunalkumar3191@gmail.com>
2021-01-05 23:05:22 -0500

    added a print statement.

--- src/practice/GitCommand.java
+++ src/practice/GitCommand.java
@@ -3,5 +3,6 @@ package practice;
 public class GitCommand {
     public static void main(String[] args) {
         System.out.println("This is git command");
+        System.out.println("print");
     }
 }
