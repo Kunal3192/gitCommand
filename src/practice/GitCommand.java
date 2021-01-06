fd967a1f99bb574f710ad060be6d0df494fd8bdc
Kunal <kunalkumar3191@gmail.com>
2021-01-06 09:28:23 -0500

    added return type in calcSum function.

--- src/practice/GitCommand.java
+++ src/practice/GitCommand.java
@@ -1,15 +1,15 @@
 package practice;
 
 public class GitCommand {
-    public static void calcSum(int a, int b){
-        System.out.println(a + b);
+    public static int calcSum(int a, int b){
+        return a + b;
     }
     public static int calcSub(int a, int b){
         return a - b;
     }
     public static void main(String[] args) {
         System.out.println("This is git command");
-        calcSum(5, 6);
+        System.out.println(calcSum(5, 6));
         System.out.println(calcSub(10, 5));
     }
 }
