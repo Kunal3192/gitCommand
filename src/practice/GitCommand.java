308a38efb090bf87503ac69cc0d058962fce52b8
Kunal <kunalkumar3191@gmail.com>
2021-01-07 08:09:48 -0500

    Revert "added function for division"
    This reverts commit b3e70938fafe0913ea9633ad91117c9ca5201c78.

--- src/practice/GitCommand.java
+++ src/practice/GitCommand.java
@@ -10,14 +10,10 @@ public class GitCommand {
     public static int calcMul(int a, int b){
         return a * b;
     }
-    public static int calcDiv(int a, int b){
-        return a / b;
-    }
     public static void main(String[] args) {
         System.out.println("This is git command");
         System.out.println(calcSum(5, 6));
         System.out.println(calcSub(10, 5));
         System.out.println(calcMul(10, 5));
-        System.out.println(calcDiv(10, 5));
     }
 }
